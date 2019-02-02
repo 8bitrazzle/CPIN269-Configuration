99cea84bf6de85a319ef1741a7b9c5660290cc99
root <root@drupal.lab.test>
2019-02-02 15:59:19 -0500

    updating drupal-boostrap.sh

--- scripts/drupal-bootstrap.sh
+++ scripts/drupal-bootstrap.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
- 
+#install basic package for bootstrap
+/usr/bin/yum -y install wget
+#install repo needed for php7 packages
+/usr/bin/rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
+
+#drupal basic setup
 /usr/bin/wget -c https://ftp.drupal.org/files/projects/drupal-8.6.7.tar.gz
 tar -zxvf drupal-8.6.7.tar.gz
 /usr/bin/mv drupal-8.6.7 /var/www/html/drupal
-/usr/bin/cd /var/www/html/drupal/sites/default/
 /usr/bin/cp /var/www/html/drupal/sites/default/default.settings.php /var/www/html/drupal/sites/default/settings.php
 /usr/bin/chown -R apache:apache /var/www/html/drupal/
 /usr/bin/rm -f ~/drupal*
