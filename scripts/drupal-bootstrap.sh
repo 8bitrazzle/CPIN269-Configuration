9e597fae2de10da6c078b7baf36c166e651b4408
root <root@drupal.test.lab>
2019-02-02 17:36:35 -0500

    fixing automation issues

--- scripts/drupal-bootstrap.sh
+++ scripts/drupal-bootstrap.sh
@@ -3,6 +3,7 @@
 #install basic package for bootstrap
 /usr/bin/yum -y install wget
 #install repo needed for php7 packages
+/usr/bin/yum -y install epel-release
 /usr/bin/rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
 
 #drupal basic setup
@@ -11,5 +12,4 @@
 tar -zxvf drupal-8.6.7.tar.gz
 /usr/bin/mv drupal-8.6.7 /var/www/html/drupal
 /usr/bin/cp /var/www/html/drupal/sites/default/default.settings.php /var/www/html/drupal/sites/default/settings.php
-/usr/bin/chown -R apache:apache /var/www/html/drupal/
 /usr/bin/rm -rf ~/CPIN269-Configuration/scripts/drupal-8*
