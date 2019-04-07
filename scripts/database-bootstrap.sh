a4fef75bec537bcf0a996e4cc4a7c9c0e5f47bc4
8bitrazzle <brianew20@yahoo.com>
2019-04-07 17:12:36 -0400

    adding rpms for future custome yum repo

--- scripts/database-bootstrap.sh
+++ scripts/database-bootstrap.sh
@@ -7,6 +7,6 @@ grant all privileges on drupal.* to 'dbuser'@'localhost' identified by 'password
 MYSQL_SCRIPT
 
 #this needs to be automated later in puppet, just here so people don't have to manually enter it 
-/usr/bin/chown -R apache:apache /var/www/html
+#/usr/bin/chown -R apache:apache /var/www/html
 
 
