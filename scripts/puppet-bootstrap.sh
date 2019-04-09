aa7e894bed44616f99e889bfdaa84fb7ce948a8e
8bitrazzle <39464171+8bitrazzle@users.noreply.github.com>
2019-04-08 22:00:03 -0400

    fixing typo

--- scripts/puppet-bootstrap.sh
+++ scripts/puppet-bootstrap.sh
@@ -19,7 +19,7 @@ esac
 /usr/bin/yum update -y
 /usr/bin/yum install -y createrepo
 
-#creates custome yum repo for puppet 
+#creates customeyum repo for puppet 
 #later on this should be its own server on the network {todolist}
 if [ ! -d /repository ]; then 
     /usr/bin/mkdir /repository
